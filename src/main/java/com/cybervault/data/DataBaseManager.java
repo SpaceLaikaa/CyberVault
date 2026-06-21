d765e1029a4a822277cbaa09a88b3be7bbb753dc
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-06-21 16:59:09 +0300

    Feat: Create getAllItems method to get vault items.

--- src/main/java/com/cybervault/data/DataBaseManager.java
+++ src/main/java/com/cybervault/data/DataBaseManager.java
@@ -1,5 +1,9 @@
 package com.cybervault.data;
 
+import com.cybervault.model.LoginCredential;
+import com.cybervault.model.SecureNote;
+import com.cybervault.model.VaultItem;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +23,11 @@ public class DataBaseManager {
 
         String sqlVaultItems = "CREATE TABLE IF NOT EXISTS vault_items ("
                 + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "item_type TEXT NOT NULL,"
                 + " app_name TEXT NOT NULL,"
                 + " username TEXT,"
-                + " encrypted_password TEXT NOT NULL,"
+                + " encrypted_password TEXT,"
+                + " note_content TEXT,"
                 + " url TEXT,"
                 + " category_id INTEGER,"
                 + " FOREIGN KEY (category_id) REFERENCES categories(id)"
@@ -85,5 +91,45 @@ public class DataBaseManager {
             System.out.println(e.getMessage());
         }
         return categories;
+    }
+
+    public List<VaultItem> getAllItems(){
+        List<VaultItem> vaultItems = new ArrayList<>();
+        String sql = "SELECT * FROM vault_items";
+
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+
+            while(rs.next()){
+                String type = rs.getString("item_type");
+
+                if(type.equals("LOGIN")){
+                    int id = rs.getInt("id");
+                    String appName = rs.getString("app_name");
+                    String username = rs.getString("username");
+                    String password = rs.getString("encrypted_password");
+                    String url = rs.getString("url");
+
+                    LoginCredential login = new LoginCredential(id, appName, url, username, password);
+
+                    vaultItems.add(login);
+
+
+                }
+                else if (type.equals("NOTE")){
+                    int id = rs.getInt("id");
+                    String appName = rs.getString("app_name");
+                    String noteContent = rs.getString("note_content");
+                    String url = rs.getString("url");
+
+                    SecureNote note = new SecureNote(id, appName, url, noteContent);
+                    vaultItems.add(note);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());;
+        }
+        return vaultItems;
     }
 }
